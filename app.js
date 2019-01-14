f4a2f3a3cdd0b3ed2e70ae2dadcb5d82ff1cee0d
HackerRank <hackerrank@example.com>
2019-01-14 05:25:58 +0000

    Saved at Sun Jan 13 2019 21:25:57 GMT-0800 (Pacific Standard Time)

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var app = express();
 mongoose.connect().then(() => {
     console.log('Db connected');
 })
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
