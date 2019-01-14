4c7127438ea5990e04f414458986812643984e77
HackerRank <hackerrank@example.com>
2019-01-14 05:25:17 +0000

    Saved at Sun Jan 13 2019 21:25:17 GMT-0800 (Pacific Standard Time)

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var actor = require('./routes/actor');
 
 var app = express();
 
-// conne
+// connect to 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
