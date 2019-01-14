150c1b49f21b4450bf82c6ea8769a1dc2dee05d6
HackerRank <hackerrank@example.com>
2019-01-14 05:25:04 +0000

    Saved at Sun Jan 13 2019 21:25:03 GMT-0800 (Pacific Standard Time)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var mongoose = re
+var mongoose = require('')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
