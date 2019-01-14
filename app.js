fdd6efe4f2cde5863a5d26a716b38f7e79bd27f1
HackerRank <hackerrank@example.com>
2019-01-14 05:25:55 +0000

    Saved at Sun Jan 13 2019 21:25:55 GMT-0800 (Pacific Standard Time)

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var app = express();
 // connect to db 
 mongoose.connect().then(() => {
     console.log('Db connected');
-    
 })
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
