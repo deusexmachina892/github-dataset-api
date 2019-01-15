72f8bcbdd002f10bf5dd62cf362673f7d40f65c2
HackerRank <hackerrank@example.com>
2019-01-15 08:12:45 +0000

    Saved at Tue Jan 15 2019 00:12:45 GMT-0800 (Pacific Standard Time)

--- routes/events.js
+++ routes/events.js
@@ -66,6 +66,7 @@ router.post('/', async (req, res) => {
     repoToBeSaved = await repoToBeSaved.save();
     actorToBeSaved = await actorToBeSaved.save();
    } catch (error) {
+
        return res.status(404).send('Something went wrong');
    }
   return res.status(201).send('Event saved successfully!');
