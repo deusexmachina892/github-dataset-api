41a48057420d874cc4e5509a40ef1ec9cd3d3e37
HackerRank <hackerrank@example.com>
2019-01-15 07:57:59 +0000

    Saved at Mon Jan 14 2019 23:57:58 GMT-0800 (Pacific Standard Time)

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -2,7 +2,7 @@ module.exports = (date) => {
   const now_utc =  Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(),
   date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
 
-   d = new Date(now_utc);
+  const d = new Date(now_utc);
   d = new Date(d.getTime() - 3000000);
   var date_format_str = d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
   return date_format_str;
