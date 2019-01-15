9b6887a3f94e9a0abc3b917faea1965394d1307a
HackerRank <hackerrank@example.com>
2019-01-15 08:54:31 +0000

    Saved at Tue Jan 15 2019 00:54:30 GMT-0800 (Pacific Standard Time)

--- routes/eraseEvents.js
+++ routes/eraseEvents.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
 
+const Repo = mongoose.model('Repo');
+const Actor = mongoose.model('Actor');
+const Event = mongoose.model('Event');
+// Route related to delete events
+
+router.delete('/', async (req, res) => {
+  await Repo.deleteMany();
+  await Actor.deleteMany();
+  await Event.deleteMany();
+  return res.status(200).send('Deleted all events');
+});
 
 module.exports = router;
