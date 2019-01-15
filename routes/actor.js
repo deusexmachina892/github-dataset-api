b4de75d1c73829f8bf3669dceac5891112e9bb74
HackerRank <hackerrank@example.com>
2019-01-15 17:01:39 +0000

    Saved at Tue Jan 15 2019 09:01:39 GMT-0800 (Pacific Standard Time)

--- routes/actor.js
+++ routes/actor.js
@@ -40,7 +40,6 @@ router.get('/', async (req, res) => {
     actorJSON = Object.values(actorJSON).sort(function (x, y) {
         return y.eventLength - x.eventLength || new Date(y.latest_event) - new Date(x.latest_event) || y.login - x.login;
     });
-    
     actorJSON = actorJSON.map(actor => {
         delete actor.eventLength;
         delete actor.latest_event;
