73b520ae773ce020c7f49924f21599f64a92c6d0
HackerRank <hackerrank@example.com>
2019-01-15 11:48:27 +0000

    Saved at Tue Jan 15 2019 03:48:27 GMT-0800 (Pacific Standard Time)

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -1,10 +1,5 @@
-module.exports = (date) => {
-  const nowUtc = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(),
-  date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+module.exports = (date) =>{
+  return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
 
-  let d = new Date(nowUtc);
-  d = new Date(d.getTime() - 3000000);
-  const dateFormatStr = d.getFullYear().toString() +"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
-  return dateFormatStr;
-};
 
