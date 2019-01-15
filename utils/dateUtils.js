432548dbdb1cc235a0f326f17751a3fd43c12f19
HackerRank <hackerrank@example.com>
2019-01-15 07:59:17 +0000

    Saved at Mon Jan 14 2019 23:59:16 GMT-0800 (Pacific Standard Time)

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -4,6 +4,6 @@ module.exports = (date) => {
 
   let d = new Date(nowUtc);
   d = new Date(d.getTime() - 3000000);
-  const dateFormatStr = d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
+  const dateFormatStr = d.getFullYear().toString() +"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
   return dateFormatStr;
 }
