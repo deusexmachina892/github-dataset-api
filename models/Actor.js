b04a2b2076b487bcfa4c4c2b73cef91c74ef4b90
HackerRank <hackerrank@example.com>
2019-01-15 07:47:28 +0000

    Saved at Mon Jan 14 2019 23:47:27 GMT-0800 (Pacific Standard Time)

---
+++ models/Actor.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const actorSchema = new mongoose.Schema({
+    _id: {
+      type: Number,
+      unique: true,
+      required: true
+    },
+    login: {
+      type: String,
+      required: true
+    },
+    avatar_url: {
+      type: String,
+      required: true
+    },
+    events:[{
+      type: Number,
+      ref:'Event'
+    }],
+    repos: [{
+      type: Number,
+      ref: 'Repo'
+    }]
+});
+
+actorSchema.pre('save', function (next) {
+  // Delete the id and avoid to save it.
+  delete this.id;
+  
+  next();
+});
+
+actorSchema.options.toObject = actorSchema.options.toJSON = {
+  virtuals: true,
+  transform: function (doc, returned, opts) {
+    returned.id = returned._id;
+
+    delete returned._id;
+  }
+};
+
+mongoose.model('Actor', actorSchema);
