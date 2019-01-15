cb0ed74205a34148f98bccb69a8772291b1ebe85
HackerRank <hackerrank@example.com>
2019-01-15 07:51:23 +0000

    Saved at Mon Jan 14 2019 23:51:22 GMT-0800 (Pacific Standard Time)

--- routes/events.js
+++ routes/events.js
@@ -1,8 +1,70 @@
-var express = require('express');
-var router = express.Router();
+
 
 // Routes related to event
 
 
+router.get('/', async (req, res) => {
+// events sorted by id
+  const events = await Event.find({}, null, {sort: {'_id': '-1'}})
+                   .select('-__v')
+                   .populate('actor repo','-__v -events -repos');
+
+   return res.status(200).send(events);
+                         
+});
+
+
+router.post('/', async (req, res) => {
+  
+  const { error } = validateEvent(req.body);
+  if (error) return res.status(400).send('Please check the fields entered!');
+
+  const {
+      id,
+      type,
+      actor,
+      repo,
+      created_at
+  } = req.body;
+
+  let event = await Event.findById(id);
+
+  if (event) return res.status(400).send('Event with same id exists!');
+
+   event = new Event({
+    _id: id,
+    type,
+    actor: actor.id,
+    repo: repo.id,
+    created_at: formattedDate(new Date(created_at))
+});
+   
+   let repoToBeSaved = new Repo({
+     _id: repo.id,
+     name: repo.name,
+     url: repo.url,
+     owner: actor.id 
+   });
+   let actorToBeSaved = new Actor({
+      _id: actor.id,
+      login: actor.login,
+      avatar_url: actor.avatar_url
+   })
+    
+   
+    actorToBeSaved.events.push(event._id);
+    actorToBeSaved.repos.push(repoToBeSaved._id); 
+   try {
+    event = await event.save();
+    repoToBeSaved = await repoToBeSaved.save();
+    actorToBeSaved = await actorToBeSaved.save();
+   } catch (error) {
+       res.status(404).send('Something went wrong');
+   }
+  
+
+  //console.log(event);
+  return res.status(200).send('Event saved successfully!');
+});
 
 module.exports = router;
