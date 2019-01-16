df7905ff561d1bb639884e178e4f4238f830978e
HackerRank <hackerrank@example.com>
2019-01-16 03:43:50 +0000

    Saved at Tue Jan 15 2019 19:43:49 GMT-0800 (Pacific Standard Time)

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    dbPath: mongodb://github-dataset-user:github-dataset-123@ds157574.mlab.com:57574/github-dataset'
+    dbPath: 'mongodb://github-dataset-user:github-dataset-123@ds157574.mlab.com:57574/github-dataset'
 };
