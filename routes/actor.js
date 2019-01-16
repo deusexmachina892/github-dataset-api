c12146663f03fc19e5c062c1e2ba9c4397054882
HackerRank <hackerrank@example.com>
2019-01-16 03:37:08 +0000

    Saved at Tue Jan 15 2019 19:37:07 GMT-0800 (Pacific Standard Time)

--- routes/actor.js
+++ routes/actor.js
@@ -36,7 +36,7 @@ router.get('/', async (req, res) => {
           actorJSON[actorId].eventLength += 1;
        }
     });
-    actorJSON = Object.values(actorJSON).sort( function (x, y) {
+    actorJSON = Object.values(actorJSON).sort(function (x, y) {
         const sortByEventLength = x.eventLength > y.eventLength ?  - 1 : (x.eventLength < y.eventLength) ? 1 : 0;
         const sortByLatestEvent = (new Date(x.latest_event) > new Date(y.latest_event))? -1 : (new Date(x.latest_event) < new Date(y.latest_event))? 1: 0;
         const sortByLogin = x.login < y.login ? -1 : (x.login > y.login)? 1:0;
