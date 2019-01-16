c92a6f0249fa2cdf01d3ae18bf4251d4441c700d
HackerRank <hackerrank@example.com>
2019-01-16 03:44:34 +0000

    Saved at Tue Jan 15 2019 19:44:33 GMT-0800 (Pacific Standard Time)

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const { dbPath } = require('./con')
+const { dbPath } = require('./config/')
 
 require('./models/Events');
 require('./models/Actor');
