c287a5ed9e75ee5b7b531a681869bd8090e6a807
HackerRank <hackerrank@example.com>
2019-01-17 15:49:14 +0000

    Saved at Thu Jan 17 2019 07:49:14 GMT-0800 (Pacific Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -12,7 +12,7 @@ const getAllEvents = async (req, res) => {
 const addEvent = async (req, res) => {
 	const { error } = validateEvent(req.body);
 	if (error) {
-		return res.status(400).send('Please check the fields entered!');
+	return res.status(400).send('Please check the fields entered!');
 	}
 	const {
 		id,
