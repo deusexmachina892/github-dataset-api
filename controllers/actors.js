b3d76f11c4e281c7e2a101b04acdcc03a92bc9c5
HackerRank <hackerrank@example.com>
2019-01-17 15:45:17 +0000

    Saved at Thu Jan 17 2019 07:45:16 GMT-0800 (Pacific Standard Time)

--- controllers/actors.js
+++ controllers/actors.js
@@ -38,7 +38,7 @@ const updateActor = async (req, res) => {
 	let actorInEvent = (await Events.findOne({ 'actor._id': id })).actor;
 	if (!actorInEvent) return res.status(404).send('Actor does not exist!');
 	if (actorInEvent.login !== login) return res.status(400).send('Cannot modify any other field that avatar_url');
- 
+
 	let actor = await Events.updateMany({'actor._id': id}, {
 		$set: {
 			'actor.avatar_url': avatar_url
